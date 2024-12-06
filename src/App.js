f2ac610d0fa082ea8cb181add54419d9b7bf0035
Tooru Wu <tom123463@icloud.com>
2024-12-06 16:47:02 +0800

    little adjest

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Clock from './clock';
 function App() {
   return (
     <div className="App w-screen h-screen">
-      <div className="w-screen h-5/6 bg-slate-100 flex flex-col items-center">     
+      <div className="w-screen h-full bg-slate-100 flex flex-col items-center">     
       <Clock />   
       <WorkingDesk />
       </div>
--- src/workingDesk.jsx
+++ src/workingDesk.jsx
@@ -4,9 +4,10 @@ import MissionBoard from "./missionBoard"
 import CreateNote from "./noteBoard"
 
 function WorkingDesk(){
+
     return(
         <div className="w-10/12 min-h-75p flex rounded-lg bg-slate-500 gap-x-1 p-1">
-            <div className="w-1/3 rounded-lg bg-slate-300 overflow-y-scroll">
+            <div className="w-1/3 flex flex-col gap-4 rounded-lg bg-slate-300 overflow-y-scroll">
                 <WeatherBoard locationName="高雄市" />
                 <CreateCalender />
             </div>
